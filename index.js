1b7411dbdf67cd9add69a06ae361863d2f7f91a3
Shai Goldberg-Kellman <sgoldbergkellman@gmail.com>
2018-04-16 22:14:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ function add2(n) {
 
 
 function funkyFunction () {
-  return function () {
-    return "FUNKY!"
+  return function funky () {
+    return 'FUNKY!'
   }
 }
 var theFunk = funkyFunction ()
